24ee0f220632f260e6d7030f20ffc56e18cfe9f6
bsu1313 <43815294+bsu1313@users.noreply.github.com>
2021-05-22 11:41:11 +0900

    First file of detection_billiard
    This code help you to detect the billiard and its corners.

---
+++ app/src/main/cpp/detection_billiard/basic.h
@@ -0,0 +1,15 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp> 
+#include <opencv2/imgproc.hpp>
+//#include <opencv2/features2d.hpp>
+//#include <opencv2/xfeatures2d.hpp>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <utility>
+
+using namespace cv;  
+using namespace std;  
---
+++ app/src/main/cpp/detection_billiard/main.cpp
@@ -0,0 +1,9 @@
+#include "basic.h"
+#include "function.h"
+
+int main(){ 
+
+    Detection_Main();
+
+    return 0;
+}
